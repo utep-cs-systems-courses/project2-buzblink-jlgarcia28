db72fa5a345ddb3af5102936f145c5411eb2ef01
Judith Garcia <jlgarcia28>
2020-10-29 19:15:26 -0600

    Buzzer and switches added.

--- project/main.c
+++ project/main.c
@@ -2,11 +2,15 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
+#include "buzzer.h"
+#include "switches.h"
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
   enableWDTInterrupts();	/* enable periodic interrupt */
-
+  buzzer_init();
+  //buzzer_set_period(2000);	/* start buzzing!!! */
+  switch_init();
   or_sr(0x18);		/* CPU off, GIE on */
 }
